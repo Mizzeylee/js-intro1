b5f6df89c00989659239750785aea1a5839d900a
Ademide Haffner <ademidehaffner@gmail.com>
2022-06-24 01:05:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Write a javascript code that prints out your name, height, and country on the screen
+
+github publsihed page is
---
+++ content.js
@@ -0,0 +1,8 @@
+let fullName = 'Ademide Stephen Haffner. <br/>';
+let height = '157cm. <br/>';
+let country = 'Nigeria. <br/>';
+
+
+document.write('Name:' + '  ' + fullName);
+document.write('Height:' + '  ' + height);
+document.write('Country:' + '  ' + country);
